ab756caa23eb7c18df0cc1b05e54b235ec3fc9b8
Horsmanayo <oniademola001@gmail.com>
2022-01-03 05:21:02 +0100

    first commit

---
+++ src/Component/Ass.js
@@ -0,0 +1,19 @@
+import React from 'react'
+// import image from './raycast-untitled.png'
+
+function Ass() {
+    return (
+        <div>
+            <div style={{border:'solid 1px black', maxWidth:'100vw'}}>
+                <h1 className='title-red'>Usman Oni</h1>
+                <br/>
+                <img src='/giorgio-trovato-fczCr7MdE7U-unsplash.jpg' alt='myimage' />
+                <br />
+                {/* <img src={image} alt ='image'/> */}
+            </div>
+            <source src='/Why Is Array_Object Destructuring So Useful And How To Use It.mp4' />
+        </div>
+    )
+}
+
+export default Ass
