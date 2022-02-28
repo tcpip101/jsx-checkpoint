3629aba09cef0e7d52726f52ca83295ef943abec
tcp101 <olaleye.dada123@gmail.com>
2022-02-28 15:46:38 +0000

    initial commit

--- src/Component/Ass.js
+++ src/Component/Ass.js
@@ -5,7 +5,7 @@ function Ass() {
     return (
         <div>
             <div style={{border:'solid 1px black', maxWidth:'100vw'}}>
-                <h1 className='title-red'>Usman Oni</h1>
+                <h1 className='title-red'>Olaleye Dada</h1>
                 <br/>
                 <img src='/giorgio-trovato-fczCr7MdE7U-unsplash.jpg' alt='myimage' />
                 <br />
